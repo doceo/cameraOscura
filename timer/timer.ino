ce961f1621c21314eeae10202a8455830d18c487
Diomede Mazzone <diomede.mazzone@gmail.com>
2024-02-05 16:08:27 +0100

    timer

--- timer/timer.ino
+++ timer/timer.ino
@@ -11,6 +11,10 @@
 #include <ctype.h>
 #include <time.h>
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
@@ -26,15 +30,36 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 int tempo = 0;
 int pinLuce = 12;
+int memTempo = 0;
+
 
 void setup(){
     Serial.begin(9600);
     pinMode(pinLuce, OUTPUT);
     digitalWrite(pinLuce, LOW);
 
+    lcd.init();
+    lcd.backlight();
+    delay(250);
+    lcd.noBacklight();
+    delay(1000);
+    lcd.backlight();
+    delay(1000);
+
 }
 
 void loop(){
+    lcd.backlight();
+
+    lcd.setCursor(0, 0);
+    lcd.print("Secondi: ");
+
+    lcd.setCursor(0, 3);
+    lcd.print("Memoria: ");
+
+    lcd.setCursor(11, 3);
+    lcd.print(memTempo);
+
     char key = keypad.getKey();
 
     if (key){
@@ -48,17 +73,23 @@ void loop(){
         //Serial.println(tempo);
 
         tempo = tempo * 10;
-        //Serial.print("moltiplico per 10: ");
-        //Serial.println(tempo);
+        Serial.print("moltiplico per 10: ");
+        Serial.println(tempo);
         
         tempo = tempo + int(key)-48;
-        //Serial.print("tempo provvisorio: ");
-        //Serial.println(tempo);
-    
+        Serial.print("tempo provvisorio: ");
+        Serial.println(tempo);
+        lcd.setCursor(0, 1);
+        lcd.print(tempo);
 
-      }else{
+
+      }else if(key == '*') {
 
         digitalWrite(pinLuce, HIGH);
+        memTempo = tempo;
+        lcd.setCursor(11, 3);
+        lcd.print(memTempo);
+        lcd.noBacklight();
 
         for(int i=0; i<tempo; i++){
           delay(1000);
@@ -66,11 +97,32 @@ void loop(){
         }
         
         tempo=0;
+        lcd.setCursor(0, 1);
+        lcd.print(tempo);
+
+      }else if (key == 'A'){
+        
+        digitalWrite(pinLuce, HIGH);
+        lcd.setCursor(11, 3);
+        lcd.print(memTempo);
+        lcd.noBacklight();
+
+        for(int i=0; i<memTempo; i++){
+          delay(1000);
+          Serial.println(i);
+        }
+        
+        tempo=0;
+        lcd.setCursor(0, 1);
+        lcd.print(tempo);
+
+        
 
       }
         digitalWrite(pinLuce, LOW);
 
       }
 
-      
+       delay(100);
+
     }
